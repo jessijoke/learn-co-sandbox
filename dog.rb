db2559c6dc38f4c7ca3a77114a65c4f5f7ee8e54
Jessica Bradham <jessijoke@yahoo.com>
2020-09-22 03:42:29 +0000

    Automatically backed up by Learn

--- dog.rb
+++ dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def name=(dog_name)
+  def initialize(dog_name)
     @this_dogs_name = dog_name
   end
   
@@ -9,16 +9,51 @@ class Dog
   
 end
 
-fido = Dog.new
-fido.name = "Fido"
+fido = Dog.new("Fido")
+puts fido.name 
 
-snoopy = Dog.new
-snoopy.name = "Snoopy"
+snoopy = Dog.new("Snoopy")
+puts snoopy.name 
 
 
-lassie = Dog.new
-lassie.name = "Lassie"
+lassie = Dog.new("Lassie")
+puts lassie.name 
 
-puts fido.name
-puts snoopy.name
-puts lassie.name
+class Person
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+  
+  def full_name
+    puts "#{@first_name} #{@last_name}"
+  end
+end
+
+taylorswift = Person.new("Taylor", "Swift")
+puts taylorswift.full_name
+
+class Pet
+  def initialize(pet_name, pet_size, pet_gender, pet_breed, pet_age, pet_color)
+    @pet_name = pet_name
+    @pet_size = pet_size
+    @pet_gender = pet_gender
+    @pet_breed = pet_breed
+    @pet_age = pet_age
+    @pet_color = pet_color
+  end
+  
+  def pet_description
+    puts "#{@pet_name} is a #{@pet_size}, #{@pet_color}, #{@pet_gender}, #{@pet_breed} who is #{@pet_age} old."
+  end
+end
+
+quiz = Pet.new("Quiz", "Large", "Female", "Saint Berdoodle", "2 yrs", "White and Gray")
+puts quiz.pet_description
+
+noodle = Pet.new("Noodle", "Large", "Female", "Newfypoo", "2 yrs", "Black and White")
+puts noodle.pet_description
+
+bigfoot = Pet.new("Bigfoot", "Large", "Male", "Newfypoo", "1 yr", "Brown")
+puts bigfoot.pet_description
